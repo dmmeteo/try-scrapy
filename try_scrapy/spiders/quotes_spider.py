f8e9cde6af6af3b3a93b7d135c16b80ae117bf58
Dmitry Sichkar <dmmeteo@gmail.com>
2018-05-09 03:31:41 +0300

    refactor code

--- try_scrapy/spiders/quotes_spider.py
+++ try_scrapy/spiders/quotes_spider.py
@@ -21,8 +21,7 @@ class QuotesSpider(scrapy.Spider):
                 "author": quote.css("small.author::text").extract_first(),
                 "tags": quote.css("div.tag a.tag::text").extract(),
             }
-        next_page = response.css('li.next a::attr(href)').extract_first()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
-
+        # for href in response.css("li.next a::attr(href)"):
+        #     yield response.follow(href, callback=self.parse)
+        for a in response.css('li.next a'):
+            yield response.follow(a, callback=self.parse)
