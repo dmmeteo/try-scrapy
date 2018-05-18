2efa8bcc0205697b6b09331807975511dceb8d5c
Dmitry Sichkar <dmmeteo@gmail.com>
2018-05-18 14:43:13 +0300

    add sitemap spider

---
+++ try_scrapy/spiders/sitemap_spider.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.spiders import SitemapSpider
+
+class SitemapSpiderSpider(SitemapSpider):
+    name = 'sitemap_spider'
+    sitemap_urls = ['http://smscoin.com/robots.txt']
+    # sitemap_urls = ['http://smscoin.com/sitemap.txt']
+    sitemap_rules = [
+        ('/shop/', 'parse_shop'),
+    ]
+
+    other_urls = ['http://smscoin.com/about']
+
+    def start_requests(self):
+        requests = list(super(SitemapSpiderSpider, self).start_requests())
+        requests += [scrapy.Request(x, self.parse_other) for x in self.other_urls]
+        return requests
+
+    def parse_shop(self, response):
+        pass # ... scrape shop here ...
+
+    def parse_other(self, response):
+        pass # ... scrape other here ...
